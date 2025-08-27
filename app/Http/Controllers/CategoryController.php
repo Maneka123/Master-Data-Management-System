a2b073db59f607e14d6b7d08659b6fe6f781fb4b
Maneka Wijesundara <maneka611997@gmail.com>
2025-08-27 18:43:30 +0530

    code to list all categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,7 +13,8 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $categories=Category::all();
+        return view('admin.category.index',compact('categories'));
     }
 
     /**
---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,44 @@
+@extends('admin.layouts.master')
+
+@section('content')
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+             <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item active" aria-current="page">All Categories</li>
+                </ol>
+            </nav>
+            <table class="table table-dark table-striped">
+  <thead>
+    
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Code</th>
+      <th scope="col">Name</th>
+      <th scope="col">Status</th>
+      <th scope="col">Edit</th>
+      <th scope="col">Delete</th>
+    </tr>
+  </thead>
+  <tbody>
+    @if(count($categories)>0)
+    @foreach($categories as $key=>$category)
+    <tr>
+      <th scope="row">{{$key+1}}</th>
+      <td>{{$category->code}}</td>
+      <td>{{$category->name}}</td>
+      <td>{{$category->status}}</td>
+      <td><i class="fas fa-edit"></td>
+      <td><i class="fas fa-trash"></td>
+    </tr>
+    @endforeach
+    @else
+    <td>No categories to display</td>
+    @endif
+  </tbody>
+</table>
+        </div>
+    </div>
+</div>
+@endsection
